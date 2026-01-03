10437aa49aeea56387c2a75076b04c4b2b4eecd8
xdelmo <emanuele.delmo@gmail.com>
2026-01-03 15:41:50 +0100

    feat: remove unused DataTableComponent and its related files

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { HomeComponent } from './pages/home/home.component';
-import { DataTableComponent } from './components/data-table/data-table.component';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [HomeComponent, DataTableComponent],
+  declarations: [HomeComponent],
   imports: [CommonModule, DashboardRoutingModule, SharedModule],
 })
 export class DashboardModule {}
