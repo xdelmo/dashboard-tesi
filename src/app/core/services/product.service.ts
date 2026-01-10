da06f91493fa4d386e423e616596b0c9e11da921
xdelmo <emanuele.delmo@gmail.com>
2026-01-10 16:10:28 +0100

    fix: db.json and product.service.ts patch method

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -3,12 +3,15 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product.model';
 import { API_CONFIG } from '../config/api.config';
+import { notifySuccess } from '../../shared/utils/notification.operator';
+import { MessageService } from 'primeng/api';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   private http = inject(HttpClient);
+  private messageService = inject(MessageService);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(`${API_CONFIG.baseUrl}/products`);
@@ -19,9 +22,10 @@ export class ProductService {
   }
 
   updateProduct(id: string, product: Partial<Product>): Observable<Product> {
-    return this.http.patch<Product>(
-      `${API_CONFIG.baseUrl}/products/${id}`,
-      product
-    );
+    return this.http
+      .patch<Product>(`${API_CONFIG.baseUrl}/products/${id}`, product)
+      .pipe(
+        notifySuccess(this.messageService, 'Prodotto aggiornato correttamente')
+      );
   }
 }
